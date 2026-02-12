9cc9c63a09c8601eed09e5f70a6562282a0fe1fa
sahithya peddineni <speddine@student.gitam.edu>
2026-02-12 15:36:12 +0530

    Update pass statement.py

--- pass statement.py
+++ pass statement.py
@@ -2,3 +2,10 @@ for i in range(1, 6):
     if i == 3:
         pass  # placeholder, do nothing
     print("Current number:", i)
+
+OUTPUT
+Current number: 1
+Current number: 2
+Current number: 3
+Current number: 4
+Current number: 5
