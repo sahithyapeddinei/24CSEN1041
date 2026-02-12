1588075f417258372d98c991697ee2a99e5a718c
sahithya peddineni <speddine@student.gitam.edu>
2026-02-12 15:32:52 +0530

    Update and rename continue statement to continue statement.py

---
+++ continue statement.py
@@ -0,0 +1,12 @@
+for i in range(1, 8):
+    if i == 4:
+        continue
+    print("Current number:", i)
+
+OUTPUT
+Current number: 1
+Current number: 2
+Current number: 3
+Current number: 5
+Current number: 6
+Current number: 7
