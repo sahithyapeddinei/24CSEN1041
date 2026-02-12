771f7d6cc1dbe3b61faa65e21b726cc69421647e
sahithya peddineni <speddine@student.gitam.edu>
2026-02-12 15:38:39 +0530

    Fix string initialization and output formatting

--- string intialization.py
+++ string intialization.py
@@ -2,3 +2,9 @@ s = "Python"
 print("Original string:", s)
 print("Length:", len(s))
 print("First character:", s[0])
+
+OUTPUT
+s = "Python"
+print("Original string:", s)
+print("Length:", len(s))
+print("First character:", s[0])
