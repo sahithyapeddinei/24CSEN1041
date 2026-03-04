5645755564a47d148b89593c92f549057c9a0738
sahithya peddineni <speddine@student.gitam.edu>
2026-03-04 11:10:05 +0530

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,11 @@
+tuple = ("2025251725", "SAHITHYA", "MECHRAI")
+print(tuple)
+print(type(tuple))
+print(len(tuple))
+print(tuple[2])
+
+OUTPUT
+('2025251725', 'SAHITHYA', 'MECHRAI')
+<class 'tuple'>
+3
+MECHRAI
