b4f3bb5142468031afba8dae8daa3dcf7a49da59
sahithya peddineni <speddine@student.gitam.edu>
2026-03-04 10:13:28 +0530

    Create list .py

---
+++ list .py
@@ -0,0 +1,16 @@
+list_1 = ["2025251725", "SAHITHYA"]
+print(list_1)
+print("Memory location of the list_1", id(list_1))
+print(type(list_1))
+print(list_1[0])
+print(list_1[0][0])
+print(type(list_1[0]))
+
+
+OUTPUT
+['2025251725', 'SAHITHYA']
+Memory location of the list_1 139702009186752
+<class 'list'>
+2025251725
+2
+<class 'str'>
